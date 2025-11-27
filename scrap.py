276b9db8cb515d9e0899dfbd2082f02f6cb7a793
Ruben BV <rubenvenancio67@gmail.com>
2025-11-27 18:53:51 +0100

    adding user input for meail

--- scrap.py
+++ scrap.py
@@ -7,19 +7,20 @@ driver = webdriver.Edge()
 driver.get('https://glassdoor.com')
 
 #on attend pour pas faire le robot
-time.sleep(10)
-
+time.sleep(5)
 
 #GLASSDOOR
 #emailField = driver.find_element(By.ID,'')
 #emailButton -> bouton continuer avec un email  inlineUserEmail -> champ email
+
+email = input("What is your email ?")
+time.sleep(4)
 emailInput = driver.find_element(By.ID,"inlineUserEmail")
-emailInput.send_keys("rubenvenancio67@gmail.com")
+emailInput.send_keys(email)
 time.sleep(5)
 emailButton = driver.find_element(By.CLASS_NAME,"emailButton")
 emailButton.click()
+time.sleep(5)
 #jobCardField = driver.find_element(By.ID,"left-column")
 
 #jobCardField.click()
-
-time.sleep(2)
